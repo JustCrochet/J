49310540f52069d2fa60d83bfa43dc808f17bc5c
JustCrochet <loroesesattamentecosa9991@gmail.com>
2025-01-05 02:46:32 +0100

    Add files via upload

--- J/JsUncinetto.js
+++ J/JsUncinetto.js
@@ -24,4 +24,17 @@ document.addEventListener("DOMContentLoaded", () => {
             console.log("Carrello attuale:", cart);
         });
     });
+
+    document.addEventListener("DOMContentLoaded", function() {
+        const dinosauro = document.getElementById("dinosauro");
+    
+        dinosauro.addEventListener("mouseenter", function() {
+            dinosauro.classList.add("dinosauro-animate"); // Aggiunge la classe per l'animazione
+        });
+    
+        dinosauro.addEventListener("mouseleave", function() {
+            dinosauro.classList.remove("dinosauro-animate"); // Rimuove l'animazione quando il mouse esce
+        });
+    });
+    
 });
